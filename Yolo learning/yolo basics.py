506a1c7aeb90ca098e45b536f0018023dd5877c4
Dmitry <telianovd@gmail.com>
2023-10-21 03:31:39 +0500

    update
    update

---
+++ Yolo learning/yolo basics.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO #основные библиотеки
+import cv2
+model = YOLO('../Yolo-weights/yolov8n.pt')
+# сверху указан "тип размера" котоый нам скачает библиотека ультралайтс, в нашем случае нано(8версия йоло)
+results = model(r'C:\Users\HUAWEI\Desktop\Yolo-learning\Yolo learning\images\1.jpg', show=True)
+#Сверху полный путь до изображения
+cv2.waitKey(0)
+#верхняя строчка нужна чтобы изображение не сворачивалось моментально 
